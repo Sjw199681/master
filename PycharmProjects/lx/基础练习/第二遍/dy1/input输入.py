5d31895dc94daffcb4707c1aa215f71cc96caf60
sjw <2553022646@qq.com>
2022-06-12 19:43:34 +0800

    更新目录

---
+++ PycharmProjects/lx/基础练习/第二遍/dy1/input输入.py
@@ -0,0 +1,46 @@
+# 输入密码
+# password = input("输入密码：")
+# print(password)
+# # input把所有接受到的信息存储成字符串类型
+#type--查看类型
+# print(type(password))
+#转换成eval类型----转换成最相近的类型
+a="123232323"
+print(eval(a))
+print(type(eval(a)))
+
+#转换成int类型---整数类型
+b='23232323'
+print(int(b))
+print(type(int(b)))
+
+#转换成float类型---浮点类型
+c="2.323232323"
+print(float(c))
+print(type(float(c)))
+#转换成str类型---字符串类似
+d='啦啦啦啦啦阿拉'
+print(str(d))
+print(type(str(d)))
+
+#转换成list类型---列表类型
+e="[这是一个列表]"
+print(list(e))
+print(type(list(e)))
+
+#tuple 转换成一个元组
+f="(123)"
+print(tuple(f))
+print(type(tuple(f)))
+
+
+
+
+# 总结
+# 转换数据类型常⽤用的函数
+# int()
+# float()
+# str()
+# list()
+# tuple()
+# eval()
